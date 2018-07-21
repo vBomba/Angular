8d43bd1dc4c03de9bad033327ba50456f38ba27c
Vasyl Bomba <bombavasyl@gmail.com>
2018-07-21 14:19:02 +0200

    Second commit app

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+// app/app.component.ts
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Hello world</h1>'
+})
+export class AppComponent { }
---
+++ app/boot.ts
@@ -0,0 +1,5 @@
+// app/boot.ts
+import {bootstrap}    from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
